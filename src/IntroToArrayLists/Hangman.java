f0b9969b07f2b09ceefb64866f5ff3e2bd059ba2
zahmed2003 <league@WTS10.attlocal.net>
2017-12-21 20:05:17 -0800

    hangman stuff

--- src/IntroToArrayLists/Hangman.java
+++ src/IntroToArrayLists/Hangman.java
@@ -1,9 +1,12 @@
 package IntroToArrayLists;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -11,96 +14,169 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Hangman {
+public class Hangman implements KeyListener {
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
-	Stack <String> hardWord;
-	Stack <String> easyWord;
+	Stack<String> hardWord;
+	Stack<String> easyWord;
+	ArrayList<JLabel> boxes;
 	int n;
 	String wordPicked;
 	String letterCount;
-	
-	
-	
+	String lettersUp;
+	char keypressed;
+
 	Hangman() {
+		boxes = new ArrayList<JLabel>();
 		frame = new JFrame();
 		panel = new JPanel();
 		label = new JLabel();
 		letterCount = "";
 		hardWord = new Stack<String>();
 		easyWord = new Stack<String>();
-		Object[] options = {"Easy Mode", "Hard Mode"};
+		Object[] options = { "Easy Mode", "Hard Mode" };
 		n = JOptionPane.showOptionDialog(frame, "Pick your level.", "Hangman", JOptionPane.YES_NO_OPTION,
-		JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-		
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+	}
+
+	
+	void lineMaker() {
+		for (int i = 0; i < wordPicked.length(); i++) {
+			JLabel label = new JLabel();
+			label.setText("_ ");
+			panel.add(label);
+			boxes.add(label);
+		}
+		label.setText(letterCount);
+	}
+	
+	void reset() {
+		for (int i = 0; i < wordPicked.length(); i++) {
+			label = boxes.get(i);
+			panel.remove(label);
+		}
+		boxes.clear();
 	}
 	
 	void setup() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.add(panel);
+		frame.addKeyListener(this);
 		panel.add(label);
-		for(int i = 0; i< wordPicked.length(); i++) {
-			letterCount += "_ ";
-		}
-		label.setText(letterCount);
-		
+		lineMaker();
+
 		frame.pack();
 	}
-	
-	void addPuzzles() {
-		//Read from a file one line at a time
-				try {
-					BufferedReader br = new BufferedReader(new FileReader("src/IntroToArrayLists/HangmanWords.txt"));
-					
-					String line = br.readLine();
-					while(line != null){
-						hardWord.push(line);
-						line = br.readLine();
-					}
-					
-					br.close();
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				//Read from a file one line at a time
-				try {
-					BufferedReader br = new BufferedReader(new FileReader("src/IntroToArrayLists/EasyHangmanWords.txt"));
-					
-					String line = br.readLine();
-					while(line != null){
-						easyWord.push(line);
-						line = br.readLine();
-					}
-					
-					br.close();
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				if(n == 0) {
-					wordPicked = easyWord.pop();
-				}else if(n == 1) {
-					wordPicked = hardWord.pop();
-				}
-				System.out.println(wordPicked);
-				
 
-			
+	void addPuzzles() {
+		// Read from a file one line at a time
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/IntroToArrayLists/HangmanWords.txt"));
+
+			String line = br.readLine();
+			while (line != null) {
+				hardWord.push(line);
+				line = br.readLine();
+			}
+
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Read from a file one line at a time
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/IntroToArrayLists/EasyHangmanWords.txt"));
+
+			String line = br.readLine();
+			while (line != null) {
+				easyWord.push(line);
+				line = br.readLine();
+			}
+
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (n == 0) {
+			wordPicked = easyWord.pop();
+		} else if (n == 1) {
+			wordPicked = hardWord.pop();
+		}
+		System.out.println(wordPicked);
+
 	}
+
+	private void updateSpacesWithUserInput(char keyChar) {
+		for (int i = 0; i < wordPicked.length(); i++) {
+			if (wordPicked.charAt(i) == keyChar) {
+				boxes.get(i).setText("" + keyChar);
+			}
+		}
+	}
+
+	private void loadNextPuzzle() {
+		reset();
+		if(n==0) {
+			wordPicked = easyWord.pop();
+		}else if(n==1) {
+			wordPicked = hardWord.pop();
+		}
+		System.out.println("the word is now " + wordPicked);
+		lineMaker();
+	}
+	
+	private String getCurrentAnswer() {
+		StringBuffer answer = new StringBuffer();
+		for (JLabel textBox : boxes) {
+			answer.append(textBox.getText());
+		}
+		return answer.toString();
+	}
+
 	public static void main(String[] args) {
 		Hangman hangman = new Hangman();
 		hangman.addPuzzles();
 		hangman.setup();
-		
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		 keypressed = e.getKeyChar();
+		if(wordPicked.indexOf(keypressed) >= 0){
+			//System.out.println("yay");
+			updateSpacesWithUserInput(keypressed);
+		}
+		lettersUp = getCurrentAnswer();
+		if(lettersUp.equals(wordPicked)) {
+			JOptionPane.showMessageDialog(null, "Hey good job you did the thing");
+			loadNextPuzzle();
+			frame.pack();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
