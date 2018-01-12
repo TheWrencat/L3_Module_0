730bc27297a25990d75a0377983c8b91c0d65df4
TheWrencat <league@WTS-iMac-11.attlocal.net>
2018-01-11 19:59:51 -0800

    Stickstack

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,14 +1,25 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
+		Stack<Double> stickstack = new Stack<Double>();
 		
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for(int i = 0; i<100; i++) {
+			Random r=new Random();
+			double stick = r.nextDouble();
+			stick *= 100;
+			stickstack.push(stick);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		JOptionPane.showInputDialog("Write a number betewen 0 and 100");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
 		
