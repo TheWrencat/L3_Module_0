47e08c17c906faa839c0313c05984c6c020a58fb
TheWrencat <league@WTS-iMac-11.attlocal.net>
2018-01-18 19:58:44 -0800

    Started textUndoRedo, errors to fix

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -6,27 +6,46 @@ import java.util.Stack;
 import javax.swing.JOptionPane;
 
 public class IntroToStack {
-	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
-		Stack<Double> stickstack = new Stack<Double>();
-		
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+	Stack<Double> stickstack = new Stack<Double>(); 
+	Double nOne;
+	Double nTwo;
+	String sOne;
+	String sTwo;
+	Double popped;
+	void setup(){
 		for(int i = 0; i<100; i++) {
 			Random r=new Random();
 			double stick = r.nextDouble();
 			stick *= 100;
 			stickstack.push(stick);
 		}
+		sOne = JOptionPane.showInputDialog("Write a number betewen 0 and 100");
+		sTwo = JOptionPane.showInputDialog("Write another number betewen 0 and 100");
+		nOne = Double.parseDouble(sOne);
+		nTwo = Double.parseDouble(sTwo);
+		
+		for(int i = 0; i < stickstack.size(); i++){
+			popped = stickstack.pop();
+			if (popped > nOne && popped <nTwo) {
+				JOptionPane.showMessageDialog(null, popped);
+				System.out.println(popped);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		IntroToStack stacks =  new IntroToStack();
+		stacks.setup();
+		//1. Create a Stack of Doubles
+		//   Don't forget to import the Stack class
+		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		JOptionPane.showInputDialog("Write a number betewen 0 and 100");
+		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+	
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
-		
 		//   Popping elements off stack...
 		//   Elements between 65 and 75:
 		//   66.66876846
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,14 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +21,49 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	String letter;
+	String textBox;
+	Stack<String> letters = new Stack<String>();
+	void setup() {
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+		frame.pack();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		TextUndoRedo text = new TextUndoRedo();
+		text.setup();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		letter = e.getKeyChar() + "";
+		letters.push(letter);
+		for(String text: letters) {
+		textBox += letters.pop();
+		}label.setText(textBox);
+		System.out.println(letter);
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
